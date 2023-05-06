195c5cdc5059ba63256c39529f8dad78d86fa1f5
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2023-05-07 02:33:16 +0900

    add: article search get method

--- whenthen-backend/src/api/index.ts
+++ whenthen-backend/src/api/index.ts
@@ -5,5 +5,6 @@ const router: Router = express.Router();
 router.use('/mock', require('./mock'));
 router.use('/auth', require('./auth'));
 router.use('/user', require('./user'));
+router.use('/search', require('./search'));
 
 module.exports = router;
---
+++ whenthen-backend/src/api/search/index.ts
@@ -0,0 +1,75 @@
+import express, { Request, Response, Router } from 'express';
+import {
+  authProtected,
+  authUnprotected,
+  IGetUserAuthInfoRequest,
+} from '../../middlewares/auth';
+import HttpStatus from 'http-status-codes';
+import promisePool from '../../db';
+
+const router: Router = express.Router();
+
+export interface ISearchData {
+  title?: string;
+  thumbnail?: Blob;
+  detail?: string;
+  url?: string;
+  place?: string;
+  start_datetime?: Date;
+  end_datetime?: Date;
+  user_id: string;
+}
+
+router.get(
+  '/',
+  authProtected,
+  async (req: IGetUserAuthInfoRequest, res: Response) => {
+    try {
+      if (
+        req.query.type == 'detail' ||
+        req.query.type == 'title' ||
+        req.query.type == 'start_datetime' ||
+        req.query.type == 'end_datetime' ||
+        req.query.type == 'place'
+      ) {
+        if (!req.query.value)
+          throw new Error(
+            `${req.query.type} does exist, but value doesn't exist.`,
+          );
+        const [rows, _] = await promisePool.execute(
+          `SELECT * FROM ARTICLE WHERE ${req.query.type} LIKE '%${req.query.value}%' LIMIT 1000`,
+        );
+
+        const data: ISearchData[] = [];
+        if (rows.length) {
+          rows.forEach((row: any) => {
+            const searchData: ISearchData = {
+              title: row.title,
+              thumbnail: row.thumbnail,
+              detail: row.detail,
+              url: row.url,
+              place: row.place,
+              start_datetime: row.start_datetime,
+              end_datetime: row.end_datetime,
+              user_id: row.user_id,
+            };
+            data.push(searchData);
+          });
+        }
+
+        return res.status(HttpStatus.OK).json({
+          status: HttpStatus.OK,
+          data: data,
+        });
+      }
+      throw new Error('Search type is invalid.');
+    } catch (err: any) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: err instanceof Error ? err.message : String(err),
+      });
+    }
+  },
+);
+
+module.exports = router;
