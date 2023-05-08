ecf7275d8699cfc7dbf9d114a6db9ed6bbb88b41
TaeYoon <tyk117@kookmin.ac.kr>
2023-05-08 14:26:46 +0900

    fix: security issue

--- whenthen-backend/src/api/article/index.ts
+++ whenthen-backend/src/api/article/index.ts
@@ -7,7 +7,6 @@ import {
 import HttpStatus from 'http-status-codes';
 import { v4 as uuidv4 } from 'uuid';
 import multer from 'multer';
-import mysql from 'mysql2';
 import promisePool from '../../db';
 
 const router: Router = express.Router();
@@ -19,20 +18,21 @@ router.post(
   upload.single('thumbnail'),
   async (req: IGetUserAuthInfoRequest, res: Response) => {
     try {
-      let image: Buffer | undefined;
-      if (req.file) {
-        image = req.file.buffer;
-        console.log(image);
-      }
-
+      const image: Buffer | null = req.file?.buffer ?? null;
+      const values = [
+        uuidv4(),
+        req.body.title,
+        image,
+        req.body.contents,
+        req.body.eventUrl,
+        req.body.placeAddr,
+        req.body.startDate,
+        req.body.endDate,
+        req.user?.userId,
+      ];
       await promisePool.execute(
-        `INSERT INTO ARTICLE (id, title, thumbnail, detail, url, place, start_datetime, end_datetime, user_id) VALUES ('${uuidv4()}', '${
-          req.body.title
-        }',${image ? `${mysql.escape(image)}` : 'NULL'},'${
-          req.body.contents
-        }','${req.body.eventUrl}','${req.body.placeAddr}','${
-          req.body.startDate
-        }','${req.body.endDate}','${req.user?.userId}');`,
+        `INSERT INTO ARTICLE (id, title, thumbnail, detail, url, place, start_datetime, end_datetime, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        values,
       );
 
       return res.status(HttpStatus.OK).json({
